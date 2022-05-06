c7bc9d17aac080ebdf646b026a43b98d3cf1db02
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-05-06 21:50:53 +0530

    made the reviews and populated the tours and users (referenced in schema)

--- Models/reviewModel.js
+++ Models/reviewModel.js
@@ -1,29 +1,47 @@
 //review /rating /createdat/ref to tour / ref to user who wrote
 const mongoose = require('mongoose');
-const reviewSchema = new mongoose.Schema({
-  review: {
-    type: String,
-    requires: [true, "Review can't be empty"],
-  },
-  rating: {
-    type: Number,
-    min: 1,
-    max: 5,
-  },
-  tour: [
-    {
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      requires: [true, "Review can't be empty"],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    tour: {
       type: mongoose.Schema.ObjectId,
       ref: 'Tour',
+      required: [true, 'Review must belong to a tour'],
     },
-  ],
-  reviewwer: [
-    {
+    user: {
       type: mongoose.Schema.ObjectId,
-      ref: 'User',
+      ref: 'Users',
+      required: [true, 'Review must belong to a author'],
     },
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: 'name photo',
+  });
+  next();
 });
+
+const Reviewmodel = new mongoose.model('Review', reviewSchema);
+module.exports = Reviewmodel;
