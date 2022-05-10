69e80a419f766532df14803591f2d67c36f2da89
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-05-11 00:57:42 +0530

    created a nested route a review in tour will be improved soon

--- Controllers/reviewController.js
+++ Controllers/reviewController.js
@@ -10,6 +10,9 @@ exports.getallReviews = async (req, res) => {
   });
 };
 exports.postreview = async (req, res) => {
+  //INIT allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
   const createdReview = await reviewModel.create(req.body);
   return res.status(200).json({
     status: 'sucess',
